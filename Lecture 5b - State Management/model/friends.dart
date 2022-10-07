7f3d6cc961bb7d947b020791dead0503f196440b
Michael Miljanovic <michael.miljanovic@uoit.ca>
2022-10-07 17:51:16 -0400

    Lec 5b friendListWidget

--- Lecture 5b - State Management/model/friends.dart
+++ Lecture 5b - State Management/model/friends.dart
@@ -11,3 +11,19 @@ class Friend{
     return "$firstName $lastName at ($email)";
   }
 }
+
+class FriendListBLoC with ChangeNotifier{
+  List<Friend> _friends = [];
+
+  List<Friend> get friends => _friends;
+
+  set friends(newFriendList){
+    _friends = newFriendList;
+    notifyListeners();
+  }
+  addFriend(newFriend){
+    _friends.add(newFriend);
+    notifyListeners();
+  }
+
+}
---
+++ Lecture 5b - State Management/views/friend_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:lec5b/model/friends.dart';
+
+class FriendListWidget extends StatelessWidget {
+  const FriendListWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    FriendListBLoC friendListBLoC = context.watch<FriendListBLoC>();
+
+    return ListView.builder(
+      padding: EdgeInsets.all(8.0),
+        itemCount: friendListBLoC.friends.length,
+        itemBuilder: (context, index){
+          return Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text("${friendListBLoC.friends[index]}"),
+          );
+        }
+    );
+  }
+}
