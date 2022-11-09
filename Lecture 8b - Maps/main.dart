e448f95f6e6750659189704187307aacb972e520
Michael Miljanovic <michael.miljanovic@uoit.ca>
2022-11-09 15:38:50 -0500

    Lec9 start

--- Lecture 8b - Maps/main.dart
+++ Lecture 8b - Maps/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:lecture8b/constants.dart';
 import 'package:lecture8b/MapMarker.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,8 +34,19 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
+  int selectedIndex = 0;
+  final pageController = PageController();
+  var currentLocation = AppConstants.myLocation;
+
+  late MapController mapController;
+
+  @override
+  void initState(){
+    super.initState();
+    mapController = MapController();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,11 +60,12 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Stack(
         children: [
           FlutterMap(
+            mapController: mapController,
               options: MapOptions(
                 minZoom: 5,
                 maxZoom: 18,
                 zoom: 13,
-                center: AppConstants.myLocation
+                center: currentLocation
               ),
             layers: [
               TileLayerOptions(
@@ -68,8 +81,22 @@ class _MyHomePageState extends State<MyHomePage> {
                         builder: (context){
                           return Container(
                             child: IconButton(
-                              onPressed: (){},
-                              icon: Icon(Icons.location_on),
+                              onPressed: (){
+                                setState(() {
+                                pageController.animateToPage(
+                                    i,
+                                    duration: Duration(milliseconds: 500),
+                                    curve: Curves.easeInOut
+                                );
+                                  selectedIndex = i;
+                                  currentLocation = mapMarkers[i].location;
+                                  _animatedMapMove(currentLocation, 11.5);
+                                });
+                              },
+                              icon: Icon(Icons.location_on,
+                              color: selectedIndex == i ?
+                              Colors.blue : Colors.black),
+                              iconSize: 45,
                             ),
                           );
                         }
@@ -85,7 +112,15 @@ class _MyHomePageState extends State<MyHomePage> {
               bottom: 2,
               height: MediaQuery.of(context).size.height*0.3,
               child: PageView.builder(
+                controller: pageController,
                   itemCount: mapMarkers.length,
+                  onPageChanged: (value){
+                    setState(() {
+                      selectedIndex = value;
+                      currentLocation = mapMarkers[value].location;
+                      _animatedMapMove(currentLocation, 11.5);
+                    });
+                  },
                   itemBuilder: (context, index){
                     var item = mapMarkers[index];
                     return Padding(
@@ -152,5 +187,41 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
     );
+  }
+
+
+  void _animatedMapMove(LatLng destLocation, double destZoom) {
+    // Create some tweens. These serve to split up the transition from one location to another.
+    // In our case, we want to split the transition be<tween> our current map center and the destination.
+    final latTween = Tween<double>(
+        begin: mapController.center.latitude, end: destLocation.latitude);
+    final lngTween = Tween<double>(
+        begin: mapController.center.longitude, end: destLocation.longitude);
+    final zoomTween = Tween<double>(begin: mapController.zoom, end: destZoom);
+
+    // Create a animation controller that has a duration and a TickerProvider.
+    var controller = AnimationController(
+        duration: const Duration(milliseconds: 1000), vsync: this);
+    // The animation determines what path the animation will take. You can try different Curves values, although I found
+    // fastOutSlowIn to be my favorite.
+    Animation<double> animation =
+    CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn);
+
+    controller.addListener(() {
+      mapController.move(
+        LatLng(latTween.evaluate(animation), lngTween.evaluate(animation)),
+        zoomTween.evaluate(animation),
+      );
+    });
+
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.dispose();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.dispose();
+      }
+    });
+
+    controller.forward();
   }
 }
